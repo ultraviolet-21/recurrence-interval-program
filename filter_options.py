e11c15d6cc299eaa62fb9b7b4ffc0e556a8a5872
ultraviolet-21 <161163105+ultraviolet-21@users.noreply.github.com>
2024-07-30 10:59:12 -0700

    Update filter_options.py

--- filter_options.py
+++ filter_options.py
@@ -1,16 +1,21 @@
 #filter_options.py
+#Allows user to filter a list of intervals
 
 def get_most_recent(data: list[int], fraction: float) -> list[int]:
+    '''Only recent data'''
     section = round(len(data) * fraction)
     return data[len(data)-section::]
 
 def greater_than_or_equal(data: list[int], x: int | float) -> list[int]:
+    '''Filters out low outliers'''
     return list(filter(lambda n: n >= x, data))
 
 def less_than_or_equal(data: list[int], x: int | float) -> list[int]:
+    '''Filters out high outliers'''
     return list(filter(lambda n: n <= x, data))
 
 def options(data: list[int]) -> list[int]:
+    '''Allows user to decide if they want to filter the list, returns unchanged list otherwise'''
     print('Option 1: get most recent [fraction] of data')
     print('Option 2: get data over a certain value')
     print('Option 3: get data under a certain value')
