69bdde71dd895875e7800e7fd040dfea01d95c39
ultraviolet-21 <161163105+ultraviolet-21@users.noreply.github.com>
2024-07-30 11:00:55 -0700

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -1,18 +1,15 @@
 #distribution.py
+#Displays a density graph of the interval distribution.
+#A lognorm distribution is an approximation, so the user should be able to see what the data really looks like.
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-import random
 
-data_pts = list(random.choices(range(100), k=30))
 
 def make_kde(data_pts: list) -> 'plot':
+    '''Displays the graph using seaborn and matplotlib'''
     sns.kdeplot(data = data_pts, fill = True, color = 'blue')
     plt.show()
 
 
-if __name__ == '__main__':
-    make_kde(data_pts)
 
-#use this for probability:
-#https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.lognorm.html
